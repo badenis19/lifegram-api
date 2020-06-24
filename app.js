99e9b532c9e1e89680a0dbbd2f50f898bcc71e15
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-06-24 06:32:20 +0100

    server setup

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
+const express = require('express');
+// const graphqlHTTP = require('express-graphql');
+// const mongoose = require("mongoose");
+const cors = require("cors");
+// const schema = require('./schema/schema')
+require("dotenv/config");
 
 
+const port = process.env.PORT || 4000;
 
+const app = express();
 
+app.use(cors());
+
+app.listen(port, () =>
+  console.log(`listening to port`, process.env.PORT)
+);
