e7df30649797fad4caa40f357e862e7a33cb524f
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-06-24 06:46:45 +0100

    connected to mongodb

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express');
 // const graphqlHTTP = require('express-graphql');
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const cors = require("cors");
 // const schema = require('./schema/schema')
 require("dotenv/config");
-
 
 const port = process.env.PORT || 4000;
 
 const app = express();
 
 app.use(cors());
+
+mongoose.connect(
+  process.env.URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => console.log("connected to mongodb 🥭")
+);
 
 app.listen(port, () =>
   console.log(`listening to port`, process.env.PORT)
